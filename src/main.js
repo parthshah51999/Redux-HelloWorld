31fd4139f3e40aeb4eecbe5e3dcacba3a1a1353e
unknown <parthshah51999@gmail.com>
2019-07-19 23:22:57 +0530

    react hello world - state

--- src/main.js
+++ src/main.js
@@ -2,7 +2,14 @@ import React from 'react';
 import HelloWorld from './HelloWorld'
 
 export default class Main extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      text: 'React',
+    }
+  }
+
   render() {
-      return <HelloWorld text='React'/>;
+      return <HelloWorld text = { this.state.text }/>;
   }
 }
